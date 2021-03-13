d2187e372775eb3fca4d057a815bbe6c8215a1c0
sergidoce <sergi.doce@estudiantat.upc.edu>
2021-03-13 14:06:59 +0100

    #75 Implementar request de registre al Backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,38 @@ const User = require('../models/user');
 
 exports.signUp = (req, res, next) => {
 
-    const user = new User({
-        username: req.body.username,
-        name: req.body.name,
-        surnames: req.body.surnames,
-        email: req.body.email,
-        password: req.body.password,
-        birthday: req.body.birthday,
-        gender: req.body.gender,
-        profileImage: req.body.profileImage
-    });
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user){
 
-    user.save()
-        .then(result =>{
-            res.status(201).json({message: 'User created!', userId: result._id});
-        })
-        .catch(err => {
-            if(!err.statusCode){
-                err.statusCode = 500;
+                res.status(409).json({message:'The email is already being used'});
+
+                const error = new Error("The email is already being used");
+                error.statusCode = 409;
+                throw error;
             }
-            next(err);
-        }); 
+            else{
+                const user = new User({
+                    username: req.body.username,
+                    name: req.body.name,
+                    surnames: req.body.surnames,
+                    email: req.body.email,
+                    password: req.body.password,
+                    birthday: req.body.birthday,
+                    gender: req.body.gender,
+                    profileImage: req.body.profileImage
+                });
+            
+                user.save()
+                    .then(result =>{
+                        res.status(201).json({message: 'User created!', userId: result._id});
+                    })
+                    .catch(err => {
+                        if(!err.statusCode){
+                            err.statusCode = 500;
+                        }
+                        next(err);
+                    }); 
+            }
+        })
 };
