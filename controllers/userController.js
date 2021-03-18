82870f24553873f9f46f1bd9652e4f0fa1ac4827
pirata99 <58880443+pirata99@users.noreply.github.com>
2021-03-18 22:22:14 +0100

    Inici
    si

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,3 +75,39 @@ exports.logIn = (req, res, next) => {
         });
 }
 
+exports.consultaProfile = (req, res, next) => {
+
+    let loadedUser;
+
+    /*
+    User.findOne({email:req.body.email})
+        .then(user => {
+            if(!user){
+                res.status(404).json({message: "A user with this email could not be found"});
+                const error = new Error("A user with this email could not be found");
+                error.statusCode = 404;
+                throw error;
+            }
+            else{
+                loadedUser = user;
+                return loadedUser.validPassword(req.body.password);
+            }
+        })
+        .then(isEqual => {
+            if(!isEqual){
+                res.status(401).json({message: "Password is not correct!"});
+                const error = new Error("Wrong password");
+                error.statusCode = 401;
+                throw error;
+            }
+            else{
+                res.status(200).json({message: "Logged In correctly!", userId: loadedUser._id.toString()});
+            }
+        })
+        .catch(err => {
+            if(!err.statusCode)err.statusCode=500;
+            next(err);
+        });
+        */
+}
+
