fd15886e49afc7e5c4137a0c52c9c6b5635b9ecf
pirata99 <58880443+pirata99@users.noreply.github.com>
2021-04-07 23:42:30 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,5 +8,6 @@ router.post('/signup', userController.signUp);
 router.post('/login', userController.logIn);
 router.get('/getUserInfo/:userId', userController.getUserInfo);
 router.get('/checkEmail/:email', userController.checkEmail);
+router.post('/notifyAssistance', userController.notifyAssistance);
 
 module.exports = router;
